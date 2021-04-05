591efecdc78eea5b4c023bb92b944e75d8abfcbf
kolinkuang <kolinkuang@github.com>
2021-04-05 16:12:23 +0800

    88.merge

---
+++ src/88.merge/case2.js
@@ -0,0 +1,66 @@
+//给你两个有序整数数组 nums1 和 nums2，请你将 nums2 合并到 nums1 中，使 nums1 成为一个有序数组。
+//
+// 初始化 nums1 和 nums2 的元素数量分别为 m 和 n 。你可以假设 nums1 的空间大小等于 m + n，这样它就有足够的空间保存来自 nu
+//ms2 的元素。
+//
+//
+//
+// 示例 1：
+//
+//
+//输入：nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
+//输出：[1,2,2,3,5,6]
+//
+//
+// 示例 2：
+//
+//
+//输入：nums1 = [1], m = 1, nums2 = [], n = 0
+//输出：[1]
+//
+//
+//
+//
+// 提示：
+//
+//
+// nums1.length == m + n
+// nums2.length == n
+// 0 <= m, n <= 200
+// 1 <= m + n <= 200
+// -109 <= nums1[i], nums2[i] <= 109
+//
+// Related Topics 数组 双指针
+// 👍 861 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ */
+var merge = function(nums1, m, nums2, n) {
+    let p1 = m -1, p2 = n - 1;
+    let tail = m + n - 1;
+    let cur;
+    while (p1 >= 0 || p2 >= 0) {
+        if (p1 === -1) {
+            cur = nums2[p2--];
+        } else if (p2 === -1) {
+            cur = nums1[p1--];
+        } else if (nums1[p1] > nums2[p2]) {
+            cur = nums1[p1--]
+        } else {
+            cur = nums2[p2--]
+        }
+        nums1[tail--] = cur;
+    }
+};
+
+const nums1 = [1,2,3,0,0,0];
+merge(nums1, 3, [2,5,6], 3);
+console.log(nums1);
+//leetcode submit region end(Prohibit modification and deletion)
