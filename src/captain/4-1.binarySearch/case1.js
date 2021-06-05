856b08dc7b360f5d58ceb4479c55e900b475cf15
kolinkuang <kolinkuang@github.com>
2021-06-05 22:30:38 +0800

    Binary Search

--- src/captain/4-1.binarySearch/case1.js
+++ src/captain/4-1.binarySearch/case1.js
@@ -1,7 +1,7 @@
 function binarySearch(arr, n, x) {
     let head = 0, tail = n - 1, mid;
     while (head <= tail) {
-        mid = (head + tail) >> 1;
+        mid = head + Math.floor((tail - head) >> 1);
         if (arr[mid] === x) {
             return mid;
         }
@@ -15,4 +15,27 @@ function binarySearch(arr, n, x) {
     return -1;
 }
 
-console.log(binarySearch(10));
+// console.log(binarySearch(10));
+
+
+function binarySearch01(arr, n, x) {
+    let head = 0, tail = n - 1, mid;
+    while (head < tail) {
+        mid = head + Math.floor((tail - head) >> 1);
+        if (arr[mid] < x) {
+            head = mid + 1; // 查找右半部
+        } else {
+            tail = mid; // 查找左半部
+        }
+    }
+
+    console.log(`arr[${head}]=${x}`);
+    return head;
+}
+
+let arr = [5, 7, 11, 13, 15, 20, 23, 24, 27, 28], n = 10, x = 14;
+console.log(binarySearch01(arr, n, x));
+
+arr = [1, 2, 3, 5, 7, 11, 14, 16, 20, 21];
+x = 11;
+console.log(binarySearch01(arr, n, x));
