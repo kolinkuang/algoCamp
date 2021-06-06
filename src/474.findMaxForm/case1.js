39c16b0e37e00d247fdd44a0a7aff32721e53ff4
kolinkuang <kolinkuang@github.com>
2021-06-06 17:15:36 +0800

    474.findMaxForm

---
+++ src/474.findMaxForm/case1.js
@@ -0,0 +1,88 @@
+//给你一个二进制字符串数组 strs 和两个整数 m 和 n 。
+//
+//
+// 请你找出并返回 strs 的最大子集的大小，该子集中 最多 有 m 个 0 和 n 个 1 。
+//
+// 如果 x 的所有元素也是 y 的元素，集合 x 是集合 y 的 子集 。
+//
+//
+//
+//
+// 示例 1：
+//
+//
+//输入：strs = ["10", "0001", "111001", "1", "0"], m = 5, n = 3
+//输出：4
+//解释：最多有 5 个 0 和 3 个 1 的最大子集是 {"10","0001","1","0"} ，因此答案是 4 。
+//其他满足题意但较小的子集包括 {"0001","1"} 和 {"10","1","0"} 。{"111001"} 不满足题意，因为它含 4 个 1 ，大于
+//n 的值 3 。
+//
+//
+// 示例 2：
+//
+//
+//输入：strs = ["10", "0", "1"], m = 1, n = 1
+//输出：2
+//解释：最大的子集是 {"0", "1"} ，所以答案是 2 。
+//
+//
+//
+//
+// 提示：
+//
+//
+// 1 <= strs.length <= 600
+// 1 <= strs[i].length <= 100
+// strs[i] 仅由 '0' 和 '1' 组成
+// 1 <= m, n <= 100
+//
+// Related Topics 动态规划
+// 👍 415 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * @param {string[]} strs
+ * @param {number} m
+ * @param {number} n
+ * @return {number}
+ */
+var findMaxForm = function(strs, m, n) {
+    // 前缀和
+    const length = strs.length;
+    const preCount = new Array(length).fill(0).map(() => new Array(2).fill(0));
+
+    for (const n of strs[0].split('')) {
+        n ? preCount[0][1]++ : preCount[0][0]++;
+    }
+
+    for (let i = 1; i < length; i++) {
+        let countZero = 0;
+        let countOne = 0;
+        for (const n of strs[i].split('')) {
+            +n ? countOne++ : countZero++;
+        }
+        preCount[i] = [countZero, countOne];
+    }
+
+    console.log('preCount:', preCount);
+
+    let maxLength = 0;
+
+    for (let i = 0; i < length; i++) {
+        for (let j = i + 1; j < length; j++) {
+            if (preCount[j][0] - preCount[i][0] <= m ||
+                preCount[j][1] - preCount[i][1] <= n
+            ) {
+                maxLength = Math.max(maxLength, j - i);
+            }
+        }
+    }
+
+    return maxLength;
+};
+
+const input = ['10','0001','111001','1','0'];
+console.log(findMaxForm(input, 4));
+// 3
+//leetcode submit region end(Prohibit modification and deletion)
