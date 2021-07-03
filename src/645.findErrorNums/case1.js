51e162f7ebc7a14d1b426b9c086b561a974584a8
kolinkuang <kolinkuang@github.com>
2021-07-04 07:59:06 +0800

    645.findErrorNums

---
+++ src/645.findErrorNums/case1.js
@@ -0,0 +1,70 @@
+//集合 s 包含从 1 到 n 的整数。不幸的是，因为数据错误，导致集合里面某一个数字复制了成了集合里面的另外一个数字的值，导致集合 丢失了一个数字 并且 有
+//一个数字重复 。
+//
+// 给定一个数组 nums 代表了集合 S 发生错误后的结果。
+//
+// 请你找出重复出现的整数，再找到丢失的整数，将它们以数组的形式返回。
+//
+//
+//
+// 示例 1：
+//
+//
+//输入：nums = [1,2,2,4]
+//输出：[2,3]
+//
+//
+// 示例 2：
+//
+//
+//输入：nums = [1,1]
+//输出：[1,2]
+//
+//
+//
+//
+// 提示：
+//
+//
+// 2 <= nums.length <= 104
+// 1 <= nums[i] <= 104
+//
+// Related Topics 位运算 数组 哈希表 排序
+// 👍 182 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var findErrorNums = function(nums) {
+    const n = nums.length;
+    const errorNums = new Array(2).fill(0);
+    nums.sort((a, b) => a - b);
+    let prev = 0;
+    for (let i = 0; i < n; i++) {
+        const cur = nums[i];
+        if (cur === prev) {
+            errorNums[0] = cur;
+        } else if (cur - prev > 1) {
+            errorNums[1] = prev + 1;
+        }
+        prev = cur;
+    }
+
+    if (nums[n - 1] !== n) {
+        errorNums[1] = n;
+    }
+
+    return errorNums;
+};
+
+const input = [37,62,43,27,12,66,36,18,39,54,61,65,47,32,23,2,46,8,4,24,29,38,63,39,25,11,45,28,44,52,15,30,21,7,57,49,1,59,58,14,9,40,3,42,56,31,20,41,22,50,13,33,6,10,16,64,53,51,19,17,48,26,34,60,35,5];
+console.log(findErrorNums(input));
+// [39,55]
+
+// 时间复杂度：O(nlog(n))
+// 空间复杂度：O(log(n))
+// n = nums.length
+//leetcode submit region end(Prohibit modification and deletion)
